0a253b743b83846ae0d6de244ecd0596c45a004a
@shubham860 <shubham.chauhan860@gmail.com>
2018-06-21 18:11:44 +0530

    newfiles

--- others/js/script.js
+++ others/js/script.js
@@ -1,14 +1,25 @@
+var count = 0;
 function list() {
-  var item =  document.getElementsByClassName("input").value;
+  var item =  document.getElementById("ip").value;
   if(item == "")
   {
     window.alert("YOU MUST ENTER SOME TASK..");
   }
-  else{
-       var item =  document.getElementsByClassName("input").value;
-       var text = document.createTextnode(item);
-       var newitem = document.createElement("li");
-       newitem.appendChild(text);
-       document.getElementById("todolist").appendChild();
-      }
+  else
+  {
+      count += 1;
+      
+      document.getElementById("todolist").innerHTML += '<li id="'+count+'"><button type="button" onclick="del('+count+')"></button>'+item+'</li>';
+       document.getElementById("ip").value = "";
+  }
+}
+
+function del(count)
+{
+  document.getElementById(count).innerHTML = "";
+}
+
+function clear()
+{
+  document.getElementById("todolist").innerHTML = "";
 }
